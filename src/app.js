9a1baa32895599bdbe6a01deb9fb4cbe44d2ca55
Matisimo <matas.remeskevicius@gmail.com>
2020-12-13 22:38:14 +0200

    Fully working first stage of project

--- src/app.js
+++ src/app.js
@@ -75,6 +75,13 @@ App = {
         window.location.reload()
     },
 
+    toggleCompleted: async(e) => {
+        App.setLoading(true)
+        const taskId = e.target.name
+        await App.todoList.toggleCompleted(taskId)
+        window.location.reload()
+    },
+
     renderTasks: async() => {
         const taskCount = await App.todoList.taskCount()
         const $taskTemplate = $('.taskTemplate')
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -28,6 +28,14 @@ contract('TodoList', (accounts) => {
         assert.equal(event.id.toNumber(), 2)
         assert.equal(event.content, 'A new task')
         assert.equal(event.completed, false)
+    })
 
+    it('toggle task is completed event', async() => {
+        const result = await this.todoList.toggleCompleted(1)
+        const task = await this.todoList.tasks(1)
+        assert.equal(task.completed, true)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), 1)
+        assert.equal(event.completed, true)
     })
 })
